4a0977502d159944056abd3b9e6fb3b3243a51b4
lirabreno <github.lira@outlook.com>
2021-10-06 02:28:31 -0300

    fix minor issues

--- src/ListTodo.js
+++ src/ListTodo.js
@@ -24,6 +24,25 @@ const ListTodo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
 
   return todos.map((todo, index) => (
     <div
+      className={'todo-row complete'}
+      key={index}
+    >
+      <div className="todo-text" key={todo.id} onClick={() => completeTodo(todo.id)}>
+        <p>Click to mark as done!</p>
+      </div>
+      <div className='icons'>
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className='delete-icon'
+          size="32px"
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, valueText: todo.text, valueTime: todo.time })}
+          className='edit-icon'
+          size="32px"
+        />
+    </div>
+    <div
       className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
       key={index}
     >
